36625c9dd338241c8d35e1cb57547573621ec4d6
skrede <code@skrede.me>
2026-04-07 22:16:45 +0200

    Feature: Add convenience headers for IK and FK modules.
    - cartan/serial/ik.h delegates to cartan/serial/ik/ik.h
    - cartan/serial/fk.h aggregates chain types, FK, and Jacobian headers

---
+++ lib/cartan-serial-chain/include/cartan/serial/fk.h
@@ -0,0 +1,13 @@
+#ifndef HPP_GUARD_CARTAN_SERIAL_FK_H
+#define HPP_GUARD_CARTAN_SERIAL_FK_H
+
+#include "cartan/serial/chain/chain.h"
+#include "cartan/serial/chain/static_chain.h"
+#include "cartan/serial/chain/chain_concept.h"
+#include "cartan/serial/chain/kinematic_chain.h"
+
+#include "cartan/serial/fk/fk_result.h"
+#include "cartan/serial/fk/jacobian.h"
+#include "cartan/serial/fk/forward_kinematics.h"
+
+#endif
